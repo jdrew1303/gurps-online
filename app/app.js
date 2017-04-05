e87b5543a65ad69491ce7610728ce88cb200fc6e
lelabo_m <marc.lelabourier@gmail.com>
2017-04-05 17:34:11 +0200

    Base

---
+++ app/app.js
@@ -0,0 +1,42 @@
+/**
+ * Created by lelabo on 05/04/17.
+ */
+var app = angular.module('gurps-online', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    // route for the home page
+        .when('/', {
+            templateUrl : 'views/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'views/about.html',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'views/contact.html',
+            controller  : 'contactController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+app.controller('mainController', function($scope) {
+    // create a message to display in our views
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+app.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+app.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+// app.run(function() {
+// });
