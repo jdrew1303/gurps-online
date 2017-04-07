40a5eb59d5399aeaa8d45a441fa165ab92d548b4
lelabo_m <marc.lelabourier@gmail.com>
2017-04-07 18:10:25 +0200

    Login page designed

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,6 +1,46 @@
 /**
  * Created by lelabo on 07/04/17.
  */
-angular.module('gurps-online').controller('loginCtrl', function($scope) {
-    $scope.message = 'Login';
+angular.module('gurps-online').controller('loginCtrl', function($scope, $mdDialog) {
+    $scope.user = {
+        email: "",
+        password: ""
+    };
+
+    $scope.login = function (data) {
+         console.log(data);
+    };
+
+    $scope.showRegister = function (ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'views/login/register.html',
+            parent: angular.element(document.body),
+            clickOutsideToClose: true,
+            targetEvent: ev
+        }).then(function(answer) {
+            console.log(answer);
+        });
+    };
 });
+
+function DialogController($scope, $mdDialog) {
+    $scope.user = {
+        username: "",
+        email: "",
+        password: ""
+    };
+
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+
+    $scope.register = function(answer) {
+        console.log(answer);
+        $mdDialog.hide(answer);
+    };
+}
