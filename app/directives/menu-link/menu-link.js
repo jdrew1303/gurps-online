536e815261bc36d7feb7cba3624d834391c5b54f
lelabo_m <marc.lelabourier@gmail.com>
2017-04-19 12:13:24 +0200

    Menu link directive

---
+++ app/directives/menu-link/menu-link.js
@@ -0,0 +1,20 @@
+/**
+ * Created by lelabo on 15/04/17.
+ */
+angular.module('gurps-online').directive('menuLink', function () {
+    return {
+        scope: {
+            section: '='
+        },
+        templateUrl: 'directives/menu-link/menu-link.html',
+        link: function ($scope, $element) {
+            var controller = $element.parent().controller();
+
+            $scope.focusSection = function () {
+                // set flag to be used later when
+                // $locationChangeSuccess calls openPage()
+                controller.autoFocusContent = true;
+            };
+        }
+    };
+})
