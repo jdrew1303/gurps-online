9bad6b40755bd54146c654e77f274320d0c5640e
lelabo_m <marc.lelabourier@gmail.com>
2017-04-25 12:49:46 +0200

    app: Change title display system

--- app/views/menu/menu.js
+++ app/views/menu/menu.js
@@ -2,28 +2,26 @@
  * Created by lelabo on 14/04/17.
  */
 angular.module('gurps-online').controller('menuCtrl', function ($rootScope, $scope, $state, $location,
-                                                                $mdSidenav, menu, AuthService) {
+                                                                $mdSidenav, MenuService, AuthService) {
 
     this.autoFocusContent = false;
-    this.menu = menu;
+    this.menu = MenuService;
     this.status = {
         isFirstOpen: true,
         isFirstDisabled: false
     };
     this.logout = function () {
-        this.currentSection = null;
         AuthService.logout();
         $state.go('login');
     };
     this.goHome = function () {
-        this.currentSection = null;
         $state.go('app');
     };
     this.isOpen = function(section) {
-        return menu.isSectionSelected(section);
+        return MenuService.isSectionSelected(section);
     };
     this.toggleOpen = function(section) {
-        menu.toggleSelectSection(section);
+        MenuService.toggleSelectSection(section);
     };
     this.isNavOpen = function () {
         return $mdSidenav('left').isOpen() || $mdSidenav('left').isLockedOpen();
