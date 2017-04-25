6f2d18cbaba2000892b830f25399de711c94c5db
lelabo_m <marc.lelabourier@gmail.com>
2017-04-25 12:52:13 +0200

    app: character menu now display the list of character created.

--- app/views/characters/menu.js
+++ app/views/characters/menu.js
@@ -1,10 +1,16 @@
 /**
  * Created by lelabo on 20/04/17.
  */
-angular.module('gurps-online').controller('charactersMenuCtrl', function($scope, $timeout, Characters) {
+angular.module('gurps-online').controller('charactersMenuCtrl', function($scope, $state, $timeout, CharactersService,
+                                                                         MenuService) {
 
+    MenuService.currentTitle = 'Menu';
     var self = this;
-        self.isOpen = false;
+    self.isOpen = false;
+
+    $scope.goToNew = function () {
+      $state.go('app.characters.new');
+    };
 
     $scope.$watch('vm.isOpen', function(isOpen) {
         if (isOpen) {
@@ -17,9 +23,10 @@ angular.module('gurps-online').controller('charactersMenuCtrl', function($scope,
     });
 
 
-    $scope.characters = [
-        new Characters(null, "MARC LE LABOURIER", 100),
-        new Characters(null, "XAVIER SOMNIER", 50)
-
-    ];
+    $scope.characters = null;
+    CharactersService.userCharacters().then(function (success) {
+        $scope.characters = success;
+    }, function (error) {
+        console.log(error);
+    });
 });
