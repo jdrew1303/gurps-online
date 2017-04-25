e186ae94b465ddd31d4c94cd6e650236069b98e7
lelabo_m <marc.lelabourier@gmail.com>
2017-04-25 12:52:36 +0200

    app: character service

--- app/services/characters.js
+++ app/services/characters.js
@@ -1,5 +1,41 @@
 /**
  * Created by lelabo on 20/04/17.
  */
-angular.module('gurps-online').factory('characters', function () {
+angular.module('gurps-online').factory('CharactersService', function ($http, $q, $resource, global, UserService) {
+    var self = this;
+    var serviceUri = global.api_dev + "/characters";
+    var CharactersResource = $resource(serviceUri, {}, {
+        create: {
+            method: "POST",
+            url: serviceUri + "/",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        },
+        find: {
+            method: "GET",
+            url: serviceUri + "/",
+            isArray: true
+        }
+    });
+
+    this.new = function (name, exp) {
+        var deferred = $q.defer();
+        UserService.me().then(function (user) {
+            CharactersResource.create({_owner: user._id, name: name, exp: exp}).$promise
+                .then(deferred.resolve, deferred.reject);
+        }, function (error) {
+            console.log(error);
+        });
+        return deferred.promise;
+    };
+
+    this.userCharacters = function () {
+        var deferred = $q.defer();
+        CharactersResource.find().$promise.then(deferred.resolve, deferred.reject);
+        return deferred.promise;
+    };
+
+    return this;
+
 });
