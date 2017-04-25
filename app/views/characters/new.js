483019ece87909251f01a460218cc4a4ca75ef91
lelabo_m <marc.lelabourier@gmail.com>
2017-04-25 12:51:33 +0200

    app: character creation page

---
+++ app/views/characters/new.js
@@ -0,0 +1,24 @@
+/**
+ * Created by lelabo on 25/04/17.
+ */
+angular.module('gurps-online').controller('charactersNewCtrl', function($scope, $state, CharactersService, MenuService) {
+    MenuService.currentTitle = 'New';
+    $scope.character = {
+        name: "",
+        exp: 0,
+    };
+
+
+    $scope.goBack = function () {
+        $state.go('app.characters.menu');
+    };
+
+    $scope.newCharacter = function (data) {
+        CharactersService.new(data.name, data.exp)
+            .then(function (success) {
+                $state.go('app.characters.menu');
+            }, function (err) {
+                console.log(err);
+            });
+    }
+});
