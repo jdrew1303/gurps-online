b3b5abc13bd5cb7ed21b9413c6f355b898cf3adc
lelabo_m <marc.lelabourier@gmail.com>
2017-05-15 18:09:43 +0200

    Update value bar

--- app/directives/value-bar/value-bar.js
+++ app/directives/value-bar/value-bar.js
@@ -13,34 +13,48 @@ angular.module('gurps-online').directive('valueBar', ['$timeout', '$mdDialog', f
         link: function($scope, $element) {
 
             $scope.gradient = [
-                '#3F51B5',
-                '#4850AE',
-                '#524FA7',
-                '#5B4EA0',
-                '#654E9A',
-                '#6E4D93',
-                '#784C8C',
-                '#814B86',
-                '#8B4B7F',
-                '#944A78',
-                '#9E4972',
-                '#A7486B',
-                '#B14864',
-                '#BA475E',
-                '#C44657',
-                '#CD4550',
-                '#D7454A',
-                '#E04443',
-                '#EA433C',
                 '#F44336',
+                '#EA433C',
+                '#E04443',
+                '#D7454A',
+                '#CD4550',
+                '#C44657',
+                '#BA475E',
+                '#B14864',
+                '#A7486B',
+                '#9E4972',
+                '#944A78',
+                '#8B4B7F',
+                '#814B86',
+                '#784C8C',
+                '#6E4D93',
+                '#654E9A',
+                '#5B4EA0',
+                '#524FA7',
+                '#4850AE',
+                '#3F51B5',
             ];
 
-            $scope.$watch('value', function(value) {
-                var index = Math.round((100.0 - value) / 5.0);
+            function computeScore() {
+                $scope.score = Math.round(($scope.value / $scope.total) * 100.0);
+            }
+
+            function updateDisplay() {
+                computeScore();
+                var index = Math.round($scope.score / 5.0);
                 var bars = $element[0].querySelectorAll('.md-bar');
                 for (var i = 0; i < bars.length; i++) {
                     bars[i].style.backgroundColor =  $scope.gradient[index];
                 }
+            }
+
+            computeScore();
+
+            $scope.$watch('value', function(value) {
+                updateDisplay();
+            });
+            $scope.$watch('total', function(value) {
+                updateDisplay();
             });
 
             $scope.showInfo = function (template, ev) {
