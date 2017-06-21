ccaa2be5c3919ef15918096da44bc87dccc5ecdf
lelabo_m <marc.lelabourier@gmail.com>
2017-06-21 17:24:44 +0200

    back button

---
+++ app/directives/back-button/back-button.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lelabo on 21/06/17.
+ */
+app.directive('backButton', ['$window', function($window) {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            elem.bind('click', function () {
+                $window.history.back();
+            });
+        }
+    };
+}]);
